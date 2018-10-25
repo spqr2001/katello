b869ece4fcc4f8b099a6fa2a5b383667d6bed697
Michael Hofstaetter <hofstaetter@casolution.de>
2018-10-25 11:48:38 +0200

    hammer cli

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-disksize
 
 # Deploy Nodes 
-vagrant up katello.local && vagrant up node01.local node02.local 
+vagrant up katello.local && vagrant up node01.local node02.local && 
 
 # Add Nodes to puppet.local 
 vagrant ssh katello.local -c "sudo /opt/puppetlabs/bin/puppet cert sign --all"
